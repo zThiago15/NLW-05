cc9027a3f4a8709a992fa879e64649fea1a3aec4
zThiago15 <thiaguinhodias.15@gmail.com>
2021-04-24 02:53:37 -0300

    finalizando chat entre cliente e atendente

--- src/http.ts
+++ src/http.ts
@@ -15,6 +15,10 @@ app.set("view engine", "html");
 
 app.get("/pages/client", (request, response) => {
     return response.render("html/client.html");
+});
+
+app.get("/pages/admin", (request, response) => {
+    return response.render("html/admin.html");
 })
 
 const http = createServer(app); // Criando procotolo http
